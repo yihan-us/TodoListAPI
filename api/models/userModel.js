1a5fb23bf50ed8dcf762ab9079075b8930e72a16
Yihan (Tony) Xu <yihan.xu@gmail.com>
2018-04-07 15:50:41 -0700

    Remove admin role

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -6,7 +6,6 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
   username: {type: String, required: true, unique: true},
   password: String,
-  admin: Boolean
 });
 
 module.exports = mongoose.model('User', UserSchema);
